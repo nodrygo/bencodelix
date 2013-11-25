121679ce44edf95f6ea93080903237ed094d120b
nodrygo <yvan.godin@gmail.com>
2013-11-25 18:08:47 +0100

    change elixir release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bencodelix.Mixfile do
   def project do
     [ app: :bencodelix,
       version: "0.0.1",
-      elixir: "~> 0.10.3-dev",
+      elixir: "> 0.10.3",
       deps: deps ]
   end
 
